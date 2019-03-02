a698d9db574644f35bed59fd128a92f84f950718
VLKNKBKSZ <kabaksizvolkan@hotmail.com>
2019-03-02 12:08:45 +0100

    Info: duidelijke comment :P

--- sql/databaseScriptVersion0.01.sql
+++ sql/databaseScriptVersion0.01.sql
@@ -1,4 +1,4 @@
-#Als een admin een boek verwijderd, worden het boek ook verwijderd van elke boekenlijst.
+#Als admin een boek verwijderd, word het boek ook verwijderd van elke boekenlijst.
 
 ALTER TABLE `watleesik`.`profile_book` 
 DROP FOREIGN KEY `FKafktj8w1u4wnie8wevasq1kpa`;
